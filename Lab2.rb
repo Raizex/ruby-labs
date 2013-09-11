13d95a9c3079972335a876da809923c6c3e62a82
Justin Huffman <jwhuffman@gmail.com>
2013-09-11 12:55:40 -0600

    Wrote test for count_words

--- Lab2.rb
+++ Lab2.rb
@@ -10,10 +10,13 @@ end
 hello = HelloWorldClass.new("Justin Huffman")
 hello.sayHi
 
+
+# Part2: Palnidrome?
 def palindrome?(string)
 	string.downcase!
 	string = string[/[a-z]/]
-	return string == string.reverse
+
+	string == string.reverse
 end
 
 def palintest(number, string)
@@ -27,3 +30,14 @@ end
 palintest(1, "Abracadabra")
 palintest(2, "Madam, I'm Adam!")
 palintest(3, "A man, a plan, a canal -- Panama")
+
+
+#Part3: Count Words
+def count_words(string)
+	# your code here
+end
+
+count_words("A man, a plan, a canal -- Panama")
+	# => {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}
+count_words "Doo bee doo bee doo"
+	# => {'doo' => 3, 'bee' => 2}
